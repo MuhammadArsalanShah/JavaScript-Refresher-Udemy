7a9e3d11b91fd57aa5af3156dca99ceeeb1ba342
MuhammadArsalanShah <arsalanshah408@yahoo.com>
2024-08-20 11:39:38 +0500

    import export refresher

---
+++ assets/scripts/app.js
@@ -0,0 +1,16 @@
+// import { apiKey, xyz as content } from "./utils.js";
+// import defApiKey from "./utils.js";
+
+import * as ourUtils from "./utils.js"
+
+// console.log('apikey:',apiKey);
+// console.log('xyz:',xyz);
+// console.log('xyz content:',content);
+
+
+// console.log('defaul apikey:', defApiKey);
+
+console.log('apikey:',ourUtils.apiKey);
+console.log('xyz:',ourUtils.xyz);
+
+console.log('default apikey:', ourUtils.default);
---
+++ assets/scripts/utils.js
@@ -0,0 +1,7 @@
+export let apiKey = 'asadkjlkjasdf2ssdf3';
+export let xyz = 'xyz';
+
+export default 'asadkjlkjasdf2ssdf3'; //can use name but cant use variable keyword
+
+// export default apiKey2 = 'asadkjlkjasdf2ssdf3';
+
