3c07ef00b9ef47f941918ab74876027dda390061
MuhammadArsalanShah <arsalanshah408@yahoo.com>
2024-08-20 12:12:33 +0500

    Variables and values

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,16 +1,35 @@
-// import { apiKey, xyz as content } from "./utils.js";
-// import defApiKey from "./utils.js";
+// // import { apiKey, xyz as content } from "./utils.js";
+// // import defApiKey from "./utils.js";
 
-import * as ourUtils from "./utils.js"
+// import * as ourUtils from "./utils.js"
 
-// console.log('apikey:',apiKey);
-// console.log('xyz:',xyz);
-// console.log('xyz content:',content);
+// // console.log('apikey:',apiKey);
+// // console.log('xyz:',xyz);
+// // console.log('xyz content:',content);
 
 
-// console.log('defaul apikey:', defApiKey);
+// // console.log('defaul apikey:', defApiKey);
 
-console.log('apikey:',ourUtils.apiKey);
-console.log('xyz:',ourUtils.xyz);
+// console.log('apikey:',ourUtils.apiKey);
+// console.log('xyz:',ourUtils.xyz);
 
-console.log('default apikey:', ourUtils.default);
+// console.log('default apikey:', ourUtils.default);
+
+
+console.log('Javascript refresher');
+
+ let userMessage = "This is a dummy user message!";
+
+ console.log('User Message:',userMessage);
+
+ const userMessage2 = "This is a dummy user message 2!";
+
+//  userMessage2 = "new value"; //generates error
+
+ console.log('User Message:',userMessage2);
+
+ var firstName = 'Arsalan';
+
+ var firstName = "Noman"; // var can be reintialized which can cause errors 
+
+ console.log("First Name:",firstName);
