506e7bcf8c9605b8694f1e599d0c58f0687427f5
MuhammadArsalanShah <arsalanshah408@yahoo.com>
2024-08-20 18:15:20 +0500

    defining functions inside other functions

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -301,26 +301,54 @@ import * as ourUtils from "./utils.js"
 
 
 
-function handleTimeout() {
-  console.log('Timed out!');
+
+
+
+
+
+// function handleTimeout() {
+//   console.log('Timed out!');
+// }
+
+// const handleTimeout2 = () => {
+//   console.log('Timed out ... again!');
+
+// }
+
+
+// setTimeout(handleTimeout, 2000);
+// setTimeout(handleTimeout2, 3000);
+// setTimeout(() => {
+//   console.log('More timing out ... !!!');
+// }, 4000);
+
+
+// function greeter(greetFn) {
+//   greetFn();
+// }
+
+// greeter(() => console.log('Hi from greeter!'));
+
+
+
+
+
+
+function init() {
+
+  console.log("hello from init function")
+  
+  function innerGreet() {
+    console.log('Hi from inner greet function');
+  }
+
+  innerGreet();
 }
 
-const handleTimeout2 = () => {
-  console.log('Timed out ... again!');
-
-}
+init()
 
 
-setTimeout(handleTimeout, 2000);
-setTimeout(handleTimeout2, 3000);
-setTimeout(() => {
-  console.log('More timing out ... !!!');
-}, 4000);
 
 
-function greeter(greetFn) {
-  greetFn();
-}
 
-greeter(() => console.log('Hi from greeter!'));
 
